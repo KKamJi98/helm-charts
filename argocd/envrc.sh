60f138adc97785782703d63db1b6148acb1f1e91
KKamJi <xowl5460@naver.com>
2025-07-20 23:15:14 +0900

    update kube-prometheus-stack

---
+++ argocd/envrc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat << EOF
+--------------------------------------------------------
+# https://artifacthub.io/packages/helm/argo/argo-cd
+
+Helm Install Command
+
+❯ helm repo add argo https://argoproj.github.io/argo-helm
+❯ helm repo update
+❯ helm upgrade --install -n argocd argocd -f kkamji_values.yaml argo/argo-cd --version 8.1.2
+
+--------------------------------------------------------
+EOF
